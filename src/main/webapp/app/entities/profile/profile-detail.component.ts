10be5e3222e39fe4ecd12a67eae0b73d5fd5ba20
Alireza Akbari <alireza.amq@gmail.com>
2018-07-17 22:31:16 -0400

    profile-details component modified

--- src/main/webapp/app/entities/profile/profile-detail.component.ts
+++ src/main/webapp/app/entities/profile/profile-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { JhiDataUtils } from 'ng-jhipster';
-
 import { IProfile } from 'app/shared/model/profile.model';
+import { IUser, Principal } from 'app/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-profile-detail',
@@ -10,12 +11,36 @@ import { IProfile } from 'app/shared/model/profile.model';
 })
 export class ProfileDetailComponent implements OnInit {
     profile: IProfile;
+    login;
+    firstName;
+    lastName;
+    email: string;
+    // user: IUser;
 
-    constructor(private dataUtils: JhiDataUtils, private activatedRoute: ActivatedRoute) {}
+    constructor(private dataUtils: JhiDataUtils, private activatedRoute: ActivatedRoute, private principal: Principal) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ profile }) => {
             this.profile = profile;
+            this.login = this.principal.userIdentity.login;
+            this.firstName = this.principal.userIdentity.firstName;
+            this.lastName = this.principal.userIdentity.lastName;
+            this.email = this.principal.userIdentity.email;
+            // console.log(this.principal.userIdentity);
+            /*this.profileService.findByUserId(this.principal.userIdentity.id).subscribe((res: HttpResponse<IProfile>) => {
+            this.profileid = res.body.id;
+            this.roleService
+                .query({
+                    page: this.page - 1,
+                    size: this.itemsPerPage,
+                    sort: this.sort(),
+                    'profileId.equals': this.profileid
+                })
+                .subscribe(
+                    (res1: HttpResponse<IRole[]>) => this.paginateRoles(res1.body, res1.headers),
+                    (res1: HttpErrorResponse) => this.onError(res1.message)
+                );
+            });*/
         });
     }
 
