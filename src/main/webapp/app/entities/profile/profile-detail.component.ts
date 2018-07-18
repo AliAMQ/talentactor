be72f38d9f93807fc99c7bbcc8f57ee18410ab56
Alireza Akbari <alireza.amq@gmail.com>
2018-07-17 22:34:27 -0400

    cleaned profile-details.ts file

--- src/main/webapp/app/entities/profile/profile-detail.component.ts
+++ src/main/webapp/app/entities/profile/profile-detail.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 import { JhiDataUtils } from 'ng-jhipster';
 import { IProfile } from 'app/shared/model/profile.model';
 import { IUser, Principal } from 'app/core';
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-profile-detail',
@@ -26,21 +25,6 @@ export class ProfileDetailComponent implements OnInit {
             this.firstName = this.principal.userIdentity.firstName;
             this.lastName = this.principal.userIdentity.lastName;
             this.email = this.principal.userIdentity.email;
-            // console.log(this.principal.userIdentity);
-            /*this.profileService.findByUserId(this.principal.userIdentity.id).subscribe((res: HttpResponse<IProfile>) => {
-            this.profileid = res.body.id;
-            this.roleService
-                .query({
-                    page: this.page - 1,
-                    size: this.itemsPerPage,
-                    sort: this.sort(),
-                    'profileId.equals': this.profileid
-                })
-                .subscribe(
-                    (res1: HttpResponse<IRole[]>) => this.paginateRoles(res1.body, res1.headers),
-                    (res1: HttpErrorResponse) => this.onError(res1.message)
-                );
-            });*/
         });
     }
 
