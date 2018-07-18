05e5b4203c945e5ffd28ebb46afc6688068f0c98
Alireza Akbari <alireza.amq@gmail.com>
2018-07-18 18:57:05 -0400

    film list added to profile

--- src/main/webapp/app/entities/profile/profile-detail.component.ts
+++ src/main/webapp/app/entities/profile/profile-detail.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { JhiDataUtils } from 'ng-jhipster';
 import { IProfile } from 'app/shared/model/profile.model';
 import { Principal } from 'app/core';
+import { HttpResponse } from '@angular/common/http';
+import { IFilm } from 'app/shared/model/film.model';
+import { ProfileService } from '../profile/profile.service';
+import { FilmService } from 'app/entities/film/film.service';
 
 @Component({
     selector: 'jhi-profile-detail',
@@ -14,9 +18,27 @@ export class ProfileDetailComponent implements OnInit {
     firstName;
     lastName;
     email: string;
-    // user: IUser;
+    profileid: number;
+    films: IFilm[];
 
-    constructor(private dataUtils: JhiDataUtils, private activatedRoute: ActivatedRoute, private principal: Principal) {}
+    constructor(
+        private dataUtils: JhiDataUtils,
+        private activatedRoute: ActivatedRoute,
+        private principal: Principal,
+        private profileService: ProfileService,
+        private filmService: FilmService
+    ) {}
+
+    loadAllFilms() {
+        this.profileService.findByUserId(this.principal.userIdentity.id).subscribe((res: HttpResponse<IProfile>) => {
+            this.profileid = res.body.id;
+            this.filmService
+                .query({
+                    'profileId.equals': this.profileid
+                })
+                .subscribe((res1: HttpResponse<IFilm[]>) => (this.films = res1.body));
+        });
+    }
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ profile }) => {
@@ -26,6 +48,7 @@ export class ProfileDetailComponent implements OnInit {
             this.lastName = this.principal.userIdentity.lastName;
             this.email = this.principal.userIdentity.email;
         });
+        this.loadAllFilms();
     }
 
     byteSize(field) {
