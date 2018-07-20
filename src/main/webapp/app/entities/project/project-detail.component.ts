a491f88c4ee66e9ed737e9d3a03b542fae354a4b
Alireza Akbari <alireza.amq@gmail.com>
2018-07-20 12:00:47 -0400

    roles added to project detail compoonent

--- src/main/webapp/app/entities/project/project-detail.component.ts
+++ src/main/webapp/app/entities/project/project-detail.component.ts
@@ -4,18 +4,45 @@ import { JhiDataUtils } from 'ng-jhipster';
 
 import { IProject } from 'app/shared/model/project.model';
 
+import { Principal } from 'app/core';
+import { ProfileService } from 'app/entities/profile/profile.service';
+import { IProfile } from 'app/shared/model/profile.model';
+import { IRole } from 'app/shared/model/role.model';
+import { RoleService } from 'app/entities/role/role.service';
+import { HttpResponse } from '@angular/common/http';
+
 @Component({
     selector: 'jhi-project-detail',
     templateUrl: './project-detail.component.html'
 })
 export class ProjectDetailComponent implements OnInit {
     project: IProject;
+    profileId: number;
+    roles: IRole[];
 
-    constructor(private dataUtils: JhiDataUtils, private activatedRoute: ActivatedRoute) {}
+    constructor(
+        private dataUtils: JhiDataUtils,
+        private activatedRoute: ActivatedRoute,
+        private principal: Principal,
+        private roleService: RoleService,
+        private profileService: ProfileService
+    ) {}
 
     ngOnInit() {
         this.activatedRoute.data.subscribe(({ project }) => {
             this.project = project;
+        });
+        this.loadAllRoles();
+    }
+
+    loadAllRoles() {
+        this.profileService.findByUserId(this.principal.userIdentity.id).subscribe((res: HttpResponse<IProfile>) => {
+            this.profileId = res.body.id;
+            this.roleService
+                .query({
+                    'profileId.equals': this.profileId
+                })
+                .subscribe((res1: HttpResponse<IRole[]>) => (this.roles = res1.body));
         });
     }
 
